df7ceee8c0147045d998cde9f0af32173a3b2f75
ch1llay <ch1llay@yandex.ru>
2022-06-02 00:37:56 +0300

    better

--- lab6/lab6.h
+++ lab6/lab6.h
@@ -24,73 +24,159 @@
 ////
 
 
+
 struct StatsContainer {
 	int create_time;
 	int power_time;
 	int subsetAA_time;
 	int subsetBA_time;
+	int equilAA;
+	int equilBA;
 	int combining_time;
 	int intersection_time;
 	int difference_time;
 	int symmetric_difference_time;
 
+
+};
+enum Stats {
+	create_time,
+	power_time,
+	subsetAA_time,
+	subsetBA_time,
+	equilAA_time,
+	equilBA_time,
+	combining_time,
+	intersection_time,
+	difference_time,
+	symmetric_difference_time
 };
 
 class Benchmarck {
-	
+
 public:
 	int N;
+	int weightTable;
+	int GetDigitAmount(int n) {
+		return floor(log10(n) + 1);
+	}
+	int* SetOnPointers;
+	int* SetOnClass;
+	int* SetOnList;
+	int* SetOnSet;
+	int* SetStats[4] = {SetOnPointers, SetOnClass, SetOnList, SetOnSet};
+	std::string GetTime(Stats statsTime) {
+		std::string s = "";
+		for (int i = 0; i < 4; i++) {
+			int time = SetStats[i][statsTime];
+			int amountDigit = GetDigitAmount(time);
+			for (int j = 0; j < (weightTable - amountDigit) / 2; j++) {
+				s += " ";
+			}
+			s += std::to_string(time);
+			for (int j = 0; j < weightTable - amountDigit - ((weightTable - amountDigit) / 2); j++) {
+				s += " ";
+			}
+			s += "|";
+		}
+		return s;
+	}
+
+	std::string GetLine() {
+		std::string str = "\n";
+		for (int _ = 0; _ < 4; _++) {
+			str += "+";
+			for (int i = 0; i < weightTable; i++) {
+				str += "-";
+			}
+		}
+		str += "+\n";
+		return str;
+	}
+	std::string GetColumnByName(std::string name) {
+		std::string s;
+		int amountSymbols = name.length();
+		for (int j = 0; j < (weightTable - amountSymbols) / 2; j++) {
+			s += " ";
+		}
+		s += name;
+		for (int j = 0; j < weightTable - amountSymbols - ((weightTable - amountSymbols) / 2); j++) {
+			s += " ";
+		}
+		s += "|";
+		return s;
+	}
+	std::string GetTable() {
+		std::string str;
+		str += GetLine();
+		str += "|";
+		str += GetColumnByName("list(func)");
+		str += GetColumnByName("list(class");
+		str += GetColumnByName("list(std)");
+		str += GetColumnByName("set(std)");
+		str += GetLine();
+		str += "|";
+		str += GetCreateTime();
+		str += GetLine();
+		return str;
+	}
 	void RunSetOnPointers() {
-		BENCHMARK(SetOnPointers.create_time, lab2::Node* A = lab2::createSet(N, 0, N));
+		BENCHMARK(SetOnPointers[Stats::create_time], lab2::Node * A = lab2::createSet(N, 0, N));
 		lab2::Node* B = lab2::createSet(N, 0, N);
-		BENCHMARK(SetOnPointers.power_time, lab2::getLength(A));
-		BENCHMARK(SetOnPointers.subsetAA_time, lab2::isSubSet(A, A ));
-		BENCHMARK(SetOnPointers.subsetBA_time, lab2::isSubSet(B, A ));
-		BENCHMARK(SetOnPointers.combining_time, lab2::combining(A, B));
-		BENCHMARK(SetOnPointers.intersection_time, lab2::intersection(A, B));
-		BENCHMARK(SetOnPointers.difference_time, lab2::difference(A,B));
-		BENCHMARK(SetOnPointers.symmetric_difference_time, lab2::symmetricDifference(A, B));
+		BENCHMARK(SetOnPointers[Stats::power_time],					 lab2::getLength(A));
+		BENCHMARK(SetOnPointers[Stats::subsetAA_time],				 lab2::isSubSet(A, A));
+		BENCHMARK(SetOnPointers[Stats::subsetBA_time],				 lab2::isSubSet(B, A));
+		BENCHMARK(SetOnPointers[Stats::equilAA_time],				 lab2::equils(A, A));
+		BENCHMARK(SetOnPointers[Stats::equilBA_time],				 lab2::equils(B, A));
+		BENCHMARK(SetOnPointers[Stats::combining_time],				 lab2::combining(A, B));
+		BENCHMARK(SetOnPointers[Stats::intersection_time]			,lab2::intersection(A, B));
+		BENCHMARK(SetOnPointers[Stats::difference_time],			 lab2::difference(A, B));
+		BENCHMARK(SetOnPointers[Stats::symmetric_difference_time], lab2::symmetricDifference(A, B));
 	}
 	void RunSetOnClass() {
-		BENCHMARK(SetOnClass.create_time, lab3::Set A(N, 0, N));
+		BENCHMARK(SetOnClass[Stats::create_time], lab3::Set A(N, 0, N));
 		lab3::Set B(N, 0, N);
-		BENCHMARK(SetOnClass.power_time, A.getLength());
-		BENCHMARK(SetOnClass.subsetAA_time, A.isSubSet(A));
-		BENCHMARK(SetOnClass.subsetBA_time, B.isSubSet(A));
-		BENCHMARK(SetOnClass.combining_time, A.combining(B));
-		BENCHMARK(SetOnClass.intersection_time, A.intersection(B));
-		BENCHMARK(SetOnClass.difference_time, A.difference(B));
-		BENCHMARK(SetOnClass.symmetric_difference_time, A.symmetricDifference(B));
+		BENCHMARK(SetOnClass[Stats::power_time], A.getLength());
+		BENCHMARK(SetOnClass[Stats::subsetAA_time], A.isSubSet(A));
+		BENCHMARK(SetOnClass[Stats::subsetBA_time], B.isSubSet(A));
+		BENCHMARK(SetOnClass[Stats::equilAA_time], A.equils(A));
+		BENCHMARK(SetOnClass[Stats::equilBA_time], B.equils(A));
+		BENCHMARK(SetOnClass[Stats::combining_time], A.combining(B));
+		BENCHMARK(SetOnClass[Stats::intersection_time],			A.intersection(B));
+		BENCHMARK(SetOnClass[Stats::difference_time], A.difference(B));
+		BENCHMARK(SetOnClass[Stats::symmetric_difference_time], A.symmetricDifference(B));
 	}
 	void RunSetOnList() {
-		BENCHMARK(SetOnList.create_time, lab4::Set A(N, 0, N));
+		BENCHMARK(SetOnClass[Stats::create_time], lab4::Set A(N, 0, N));
 		lab4::Set B(N, 0, N);
-		BENCHMARK(SetOnList.power_time, A.getLength());
-		BENCHMARK(SetOnList.subsetAA_time, A.isSubSet(A));
-		BENCHMARK(SetOnList.subsetBA_time, B.isSubSet(A));
-		BENCHMARK(SetOnList.combining_time, A.combining(B));
-		BENCHMARK(SetOnList.intersection_time, A.intersection(B));
-		BENCHMARK(SetOnList.difference_time, A.difference(B));
-		BENCHMARK(SetOnList.symmetric_difference_time, A.symmetricDifference(B));
+		BENCHMARK(SetOnClass[Stats::power_time], A.getLength());
+		BENCHMARK(SetOnClass[Stats::subsetAA_time], A.isSubSet(A));
+		BENCHMARK(SetOnClass[Stats::subsetBA_time], B.isSubSet(A));
+		BENCHMARK(SetOnClass[Stats::equilAA_time], A.equils(A));
+		BENCHMARK(SetOnClass[Stats::equilBA_time], B.equils(A));
+		BENCHMARK(SetOnClass[Stats::combining_time], A.combining(B));
+		BENCHMARK(SetOnClass[Stats::intersection_time], A.intersection(B));
+		BENCHMARK(SetOnClass[Stats::difference_time], A.difference(B));
+		BENCHMARK(SetOnClass[Stats::symmetric_difference_time], A.symmetricDifference(B));
 	}
 	void RunSetOnSet() {
-		BENCHMARK(SetOnSet.create_time, lab5::Set A(N, 0, N));
+		BENCHMARK(SetOnClass[Stats::create_time], lab5::Set A(N, 0, N));
 		lab5::Set B(N, 0, N);
-		BENCHMARK(SetOnSet.power_time, A.getLength());
-		BENCHMARK(SetOnSet.subsetAA_time, A.isSubSet(A));
-		BENCHMARK(SetOnSet.subsetBA_time, B.isSubSet(A));
-		BENCHMARK(SetOnSet.combining_time, A.combining(B));
-		BENCHMARK(SetOnSet.intersection_time, A.intersection(B));
-		BENCHMARK(SetOnSet.difference_time, A.difference(B));
-		BENCHMARK(SetOnSet.symmetric_difference_time, A.symmetricDifference(B));
+		BENCHMARK(SetOnClass[Stats::power_time], A.getLength());
+		BENCHMARK(SetOnClass[Stats::subsetAA_time], A.isSubSet(A));
+		BENCHMARK(SetOnClass[Stats::subsetBA_time], B.isSubSet(A));
+		BENCHMARK(SetOnClass[Stats::equilAA_time], A.equils(A));
+		BENCHMARK(SetOnClass[Stats::equilBA_time], B.equils(A));
+		BENCHMARK(SetOnClass[Stats::combining_time], A.combining(B));
+		BENCHMARK(SetOnClass[Stats::intersection_time], A.intersection(B));
+		BENCHMARK(SetOnClass[Stats::difference_time], A.difference(B));
+		BENCHMARK(SetOnClass[Stats::symmetric_difference_time], A.symmetricDifference(B));
 	}
-	Benchmarck(int n) {
+	Benchmarck(int n, int weight_table) {
 		N = n;
+		weightTable = weight_table;
 	}
-	StatsContainer SetOnPointers;
-	StatsContainer SetOnClass;
-	StatsContainer SetOnList;
-	StatsContainer SetOnSet;
+
 };
 
 
--- lab6/main.cpp
+++ lab6/main.cpp
@@ -4,17 +4,13 @@ using namespace std;
 
 int main() {
 	system("color F0");
-	Benchmarck benchmarck(2000);
+	Benchmarck benchmarck(2000, 14);
 	benchmarck.RunSetOnPointers();
 	benchmarck.RunSetOnClass();
 	benchmarck.RunSetOnList();
 	benchmarck.RunSetOnSet();
 
-	cout << "+----------+------------+-------------+-----------+----------+" << endl;
-	cout << "|   name   | list(func) | list(class) | list(std) | set(std) |" << endl;
-	cout << "+----------+------------+-------------+-----------+----------+" << endl;
-	cout << "|  create  |  " << benchmarck.SetOnPointers.create_time << "  | " << benchmarck.SetOnClass.create_time << "  |  " << benchmarck.SetOnList.create_time << " |  " << benchmarck.SetOnSet.create_time << "  |";
-
+	cout << benchmarck.GetTable();
 
 	return 0;
 }
